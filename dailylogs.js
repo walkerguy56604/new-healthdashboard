d6ed2bf195de2e56a686bf701d7c2892ffd8e1ea
Mark Lindsay <godisawsume@icloud.com>
2025-12-31 09:13:51 -0800

    Create dailylogs.js

---
+++ dailylogs.js
@@ -0,0 +1,12 @@
+"2025-12-31": {
+  bloodPressure: [
+    { systolic: 130, diastolic: 69, heartRate: 80 },
+    { systolic: 121, diastolic: 67, heartRate: 80 }
+  ],
+  glucose: [{ value: 5.4 }],
+  walk: 0,
+  treadmill: 0,
+  strength: 0,
+  calories: 0,
+  heartRate: 0 // will update later if needed
+}
