a9a006146faf508d9fe3c495fbf103d3e7a49733
Mark Lindsay <godisawsume@icloud.com>
2026-01-01 12:01:18 -0800

    Update dailylogs.js

--- dailylogs.js
+++ dailylogs.js
@@ -1,13 +1,16 @@
 export const dailyLogs = {
   "2026-01-01": {
     bloodPressure: [
-      { systolic: 135, diastolic: 71, heartRate: 88, note: "5 min after strength training, high hypertension" }
+      { systolic: 117, diastolic: 69, heartRate: 88, note: "Morning, pre-exercise, low hypertension" },
+      { systolic: 135, diastolic: 71, heartRate: 88, note: "5 min after treadmill/strength training, high hypertension" }
     ],
-    glucose: [], 
-    walk: 0,     
-    treadmill: 0,
-    strength: 10,
-    calories: 11,
-    heartRate: 88
+    glucose: [
+      { value: 6.4, time: "08:15" } // morning glucose test
+    ],
+    walk: 45,       // outside walk including treadmill
+    treadmill: 10,  // 10 min treadmill
+    strength: 10,   // 10 reps
+    calories: 11,   // calories burned
+    heartRate: 111  // average HR from treadmill session
   }
 };
